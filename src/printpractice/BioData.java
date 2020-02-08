26d79c464e427d623de41579163dc31982c5db83
Heshvipatel17 <heshvipatel17@gmail.com>
2020-02-08 16:16:53 +0000

    Java practice project

---
+++ src/printpractice/BioData.java
@@ -0,0 +1,16 @@
+package printpractice;
+
+public class BioData {
+
+    public static void main(String[] args) {
+
+        System.out.println("Jitendra");
+        System.out.println("Patel");
+        System.out.println("119, whether drive, Stanmore, HA7 2HH");
+
+        System.out.print("Jitendra ");
+        System.out.print("Patel ");
+        System.out.print("119, whether drive, Stanmore, HA7 2HH");
+
+    }
+}
---
+++ src/printpractice/HelloWorld.java
@@ -0,0 +1,17 @@
+package printpractice;
+
+public class HelloWorld {
+public static void main(String[] args){
+
+    System.out.println("Hello");
+    System.out.println("World");
+
+    System.out.print("Hello ");
+    System.out.print("World");
+
+
+}
+
+
+
+}
